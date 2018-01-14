0d2c87dddf9ba1316634c2545f97cfbaf92e00c2
Paulina  Kosińska <paulinakosinska95@gmail.com>
2018-01-14 13:52:31 +0100

    add delete functionality

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-$(document).ready(function(){
-    $(".btn").on("click", function(){
+$(document).ready(function () {
+    $(".btn").on("click", function () {
         var product = $("#productName").val();
-        addRow(product, 1); 
+        addRow(product, 1);
     });
     $('#productName').keypress(function (e) {
         var key = e.which;
-        if(key == 13)  // the enter key code
-         {
-           $('#addBtn').click();
-           return false;  
-         }
-       });
-});
 
-function addRow(productName, productQuantity){
+        // the enter key code
+        if (key == 13) {
+            $('#addBtn').click();
+            return false;
+        }
+    });
+    $(document).on("click", function () {
+        $(".remove").on("click", function () {
+            remove($(this));
+        });
+    });
+});
+function remove(element) {
+    element.parent().remove();
+}
+
+function addRow(productName, productQuantity) {
 
     var row = `
     <tr>
         <td>${productName}</td>
         <td><input type="number" id="qty" class="form-control"></td>
         <td><input type="checkbox"></td>
-        <td><i class="fas fa-times"></i></td>
+        <td class="remove"><i class="fas fa-times"></i></td>
     </tr>
         `;
 
-    $('.table tr:last').after(row);//"<td>" + productName + "</td> <td>" + <input type=\"number\"> + "</td> <td><input type=\"checkbox\"> </td>")
+    $('.table tr:last').after(row);
 
 }
